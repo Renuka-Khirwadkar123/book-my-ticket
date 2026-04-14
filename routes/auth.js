8c45c42cb8de7b7e694e4be51fabc0ee85e0b10a
Renuka Khirwadkar <renuka.khirwadkarr@gmail.com>
2026-04-14 22:50:55 +0530

    done

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { register, login } = require("../controllers/auth");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 router.post("/register", register);
 router.post("/login", login);
